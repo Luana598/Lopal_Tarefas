26d3921acff464da53aa538b550a70cf72399449
LUANA MARIANA LOPES BOMFIM <25132776@CFP127ALUNO.LOCAL>
2025-05-12 11:21:12 -0300

    estrutura de classes iniciada

---
+++ src/br/dev/luana/tarefas/Main.java
@@ -0,0 +1,75 @@
+package br.dev.luana.tarefas;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import br.dev.luana.tarefas.model.Tarefa;
+
+public class Main {
+	//determinando o caminho do arquivo que será lido
+	private static String path = "C:\\Users\\25132776\\tarefa\\tarefas.txt";
+
+	public static void main(String[] args) {
+		
+		
+		Tarefa t = new Tarefa();
+		
+		gravarArquivo();
+		lerArquivo();
+	
+	}
+	
+	private static void gravarArquivo() {
+		FileWriter arquivo = null;
+		BufferedWriter escritor = null;
+		
+		try {
+			//true = append (adicionar)
+			arquivo = new FileWriter(path, true);
+			escritor = new BufferedWriter(arquivo);
+			
+			escritor.write("Essa é a ultima linha!!!!! (será?) \n");
+			escritor.flush();
+			
+		} catch (Exception erro) {
+			System.out.println(erro.getMessage());
+		}
+	}
+
+	private static void lerArquivo() {
+
+		//abrir o arquivo para leitura 
+		FileReader file = null;
+		BufferedReader buffer = null;
+		
+		try {
+		file = new FileReader(path);
+		buffer = new BufferedReader(file);
+		
+		String linha ="";
+		
+		while (linha != null) {
+			linha = buffer.readLine();
+			if (linha != null) {
+			System.out.println(linha);
+			}
+		}
+
+		
+		} catch (FileNotFoundException erro) {
+			System.out.println("Arquivo não encontrado!");
+			System.out.println(erro.getMessage());
+		}catch (IOException erro) {
+			System.out.println("Você não pode ler o arquivo!");
+			System.out.println(erro.getMessage());
+		}catch (Exception erro) {
+			System.out.println("Erro genérico");
+			System.out.println(erro.getMessage());
+		}
+	}
+
+}
---
+++ src/br/dev/luana/tarefas/model/Status.java
@@ -0,0 +1,8 @@
+package br.dev.luana.tarefas.model;
+
+public enum Status {
+	
+	 NAO_INICIADA, EM_ANDAMENTO, FINALIZADO, EM_ATRASO
+	 
+
+}
